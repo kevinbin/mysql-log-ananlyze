763ffabfb06c26d4675e73bad69bb25edb69b31b
kevinbin <hongbin119@gmail.com>
2012-08-07 11:26:55 +0800

    添加check-ssh函数

--- log_collect.sh
+++ log_collect.sh
@@ -25,68 +25,84 @@ mysqladmin=`which mysqladmin`
 
 
 # function list
-function memsage() {
+function memsage {
   local logdate=`date "+%Y_%m_%d %H:%M:%S"`
   printf "$logdate \033[32m[ INFO ]\033[0m $@ \033[32m[OK]\033[0m\n" |tee -a $sys_log
 }
 
-function die() {
+function die {
   local logdate=`date "+%Y_%m_%d %H:%M:%S"`
   printf "$logdate \033[3m[ ERROR ]\033[0m $@ \033[31m[FAILD]\033[0m\n" |tee -a $sys_log; exit 2
 }
 
-function mysql_local_connect() {
+function mysql_local_connect {
   $mysql -ss -S $sock -p$local_passwd $log_database -e "$@"
 }
 
-function mysql_remote_connect() {
+function mysql_remote_connect {
   $mysql -ss -u$user -p$passwd -h$desthost -P$port mysql -e "$@"
 }
 
-function check_mysql_client() {
+function check_mysql_client {
   [ ! -d $log_dir/history ] && mkdir -p $log_dir/history
   [ ! -x $mysql ] && die "File $mysql does not exists."
   [ ! -x $mysqladmin ] && die " File $mysqladmin does not exists."
 }
 
-function check_mysql_alive() {
+function check_mysql_alive {
   memsage "Check [ $desthost ] mysql is alive ?"
   $mysqladmin -u$user -p$passwd -h$desthost -P$port ping > /dev/null
   [ $? != 0 ] && die "target mysql host not alive"
 }
 
-function check_engine() {
+function check_engine {
   memsage "Check [ $desthost ] general_log table is myisam engine ?"
   engine=`$mysql -ss -u$user -p$passwd -h$desthost -P$port mysql -e "select engine from information_schema.tables where table_schema='mysql' and table_name='general_log'"`
   [ $engine != "MyISAM" ] && die "general_log not is myisam engine"
 }
 
-function backup_general_log() {
+function backup_general_log {
   # 备份general_log表为general_log_bak
   memsage "Rename general_log to general_log_bak on [ $desthost ] ... "
   backup_general_sql="drop table if exists general_log_bak;create table if not exists tmp_general_log like general_log;set global general_log=off;rename table general_log to general_log_bak, tmp_general_log to general_log;set global general_log=on"
   mysql_remote_connect "$backup_general_sql"
 }
+function check_ssh {
+  exit_on_error=1
+  mkdir -p  $HOME/.ssh
+  printf "trying to ssh_keyscan $desthost: "
+  ssh-keyscan -trsa $desthost  >> $HOME/.ssh/known_hosts 2>&1
+  if [ $? -eq 0 ]; then
+        printf "\033[32m[ok]\033[0m\n"
+        return 0
+  else
+        memsage "${desthost} ssh-keyscan failed - aborting current operation"
+        if [ -n "$exit_on_error" ]; then
+           exit 1
+        fi
+        return 1
+  fi
+}
 
-function tar_general_log() {
+function tar_general_log {
   # 打包general_log_bak
   memsage "Compress [ ${backupdate}_general_log_bak.tar.gz ] on [ $desthost ] ... "
   mysqldata=`$mysql -ss -u$user -p$passwd -h$desthost -P$port -e "select VARIABLE_VALUE from information_schema.global_variables where variable_name='datadir';"`
   ssh -q -tt $os_user@$desthost "cd $mysqldata/mysql/ && tar czf /tmp/${backupdate}_general_log_bak.tar.gz ./general_log_bak*"
 }
 
-function remote_copy() {
+function remote_copy {
   memsage "Remote copy [ ${backupdate}_general_log_bak.tar.gz ] to local [ $log_dir ] ... "
   scp -q $os_user@$desthost:/tmp/${backupdate}_general_log_bak.tar.gz $log_dir
 }
 
-function clean_file() {
+function clean_file {
   memsage "Cleanup [ /tmp/${backupdate}_general_log_bak.tar.gz ] on [ $desthost ] ... "
   ssh -q -tt $os_user@$desthost "rm -f /tmp/${backupdate}_general_log_bak.tar.gz "
 
 }
 
-function extract_file() {
+function extract_file {
   mysql_datadir=`$mysql -ss -S $sock -p$local_passwd $log_database -e "select VARIABLE_VALUE from information_schema.global_variables where variable_name='datadir';"`
   memsage "Extracting [ ${backupdate}_general_log_bak.tar.gz ] to [ $mysql_datadir/$log_database ] ... "
   tar xf $log_dir/${backupdate}_general_log_bak.tar.gz -C $mysql_datadir/$log_database
@@ -94,7 +110,7 @@ function extract_file() {
   [ ! -e  $mysql_datadir/$log_database/general_log_bak.frm ] && die "Table strucate file does not exists."
 }
 
-function dump_history_log() {
+function dump_history_log {
   memsage "Dumping [ $log_dir/history/$general_log ] ..."
   gather_sql="select event_time,substring_index(concat_ws('@',substring_index(user_host,'[',1),substring_index(user_host,'[',-1)),']',1),replace(argument,'\`','') from general_log_bak where command_type='query' and argument regexp '^select.*from|^insert|^update|^delete' and user_host not regexp '^log_analyse';"
   mysql_local_connect "$gather_sql" | awk -F '\t' 'BEGIN{IGNORECASE=1;OFS="\t"}{sub(/@/,"\t")}{if ($4 ~ /^select/){print "",0,$0};if ($4 !~ /^select/){print "",1,$0}}' > $log_dir/history/$general_log
@@ -111,33 +127,33 @@ function import_history_log_day {
   mysql_local_connect "create table if not exists general_log_`date +%Y_%m_%d` like general_log;load data infile '$log_dir/history/$general_log' into table general_log_`date +%Y_%m_%d`" &
 }
 
-function dump_history_select_log() {
+function dump_history_select_log {
   memsage "Dumping [ $log_dir/history/$select_log ] ..."
   select_info="select event_time,user,host,info_statement from general_log_$backupdate where sql_type = '0';"
   [ ! -d $log_dir/select_history ] && mkdir -p $log_dir/select_history
   mysql_local_connect "$select_info"  > $log_dir/select_history/$select_log
 }
 
-function dump_history_modify_log() {
+function dump_history_modify_log {
   memsage "Dumping [ $log_dir/history/$modify_log ] ..."
   modify_info="select event_time,user,host,info_statement from general_log_$backupdate where sql_type != '0';"
   [ ! -d $log_dir/modify_history ] && mkdir -p $log_dir/modify_history
   mysql_local_connect "$modify_info"  > $log_dir/modify_history/$modify_log
 }
 
-function dump_select_log() {
+function dump_select_log {
   memsage "Dumping [ $log_dir/$select_log ] ..."
   select_info="select '',event_time,user,host,info_statement from general_log_$backupdate where sql_type = '0' and user != '$webuser';"
   mysql_local_connect "$select_info"  > $log_dir/$select_log
 }
 
-function dump_modify_log() {
+function dump_modify_log {
   memsage "Dumping [ $log_dir/$modify_log ] ..."
   modify_info="select '',event_time,user,host,info_statement from general_log_$backupdate where sql_type != '0' and user != '$webuser';"
   mysql_local_connect "$modify_info"  > $log_dir/$modify_log
 }
 
-function map_select_log () {
+function map_select_log  {
   memsage "Collecting select keyword to [ $select_keyword ] file ..."
   map_select_sql="select GROUP_CONCAT(DISTINCT(column_name)),column_id from attention_columns where select_checked=1 group by column_name"
   mysql_local_connect "$map_select_sql" | awk '{printf("keyword=%s keyid=%s\n", $1,$2)}' > $select_keyword
@@ -150,7 +166,7 @@ function map_select_log () {
   done
 }
 
-function map_modify_log () {
+function map_modify_log  {
   memsage "Collecting modify keyword to [ $modify_keyword ] file ..."
   map_modify_sql="select GROUP_CONCAT(DISTINCT(column_name)),column_id from attention_columns where modify_checked=1 group by column_name"
   mysql_local_connect "$map_modify_sql" | awk '{printf("keyword=%s keyid=%s\n", $1,$2)}' > $modify_keyword
@@ -163,27 +179,27 @@ function map_modify_log () {
   done
 }
 
-function reduce_select_log() {
+function reduce_select_log {
   memsage "Reducing [ $log_dir/$select_log ] file ..."
   awk -F '\t' '$1!=""{print "\t"$0}' $log_dir/$select_log > $log_dir/select_$$.tmp && mv $log_dir/select_$$.tmp $log_dir/$select_log
 }
 
-function import_select_log() {
+function import_select_log {
   memsage "Import [ $log_dir/$select_log ] file ..."
   mysql_local_connect "load data infile '$log_dir/$select_log' into table select_log " &
 }
 
-function reduce_modify_log() {
+function reduce_modify_log {
   memsage "Reducing [ $log_dir/$modify_log ] file ..."
   awk -F '\t' '$1!=""{print "\t"$0}' $log_dir/$modify_log > $log_dir/modify_$$.tmp && mv $log_dir/modify_$$.tmp $log_dir/$modify_log
 }
 
-function import_modify_log() {
+function import_modify_log {
   memsage "Import [ $log_dir/$modify_log ] file to modify_log table ..."
   mysql_local_connect "load data infile '$log_dir/$modify_log' into table modify_log " &
 }
 
-function clean_tmpfile() {
+function clean_tmpfile {
   memsage "Cleanup tmp file on localhost ... "
   mysql_local_connect "drop table if exists general_log_bak;drop table if exists general_log_$backupdate"
   rm -f /tmp/tmp.*
